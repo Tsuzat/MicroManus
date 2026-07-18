6c24ad3fd3da337fddc8a0e1dde8039e7452c79f
Tsuzat <dev.tsuzat@gmail.com>
2026-07-19 01:11:32 +0530

    first commit

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,50 @@
+import { env } from '$env/dynamic/private';
+import { betterAuth } from 'better-auth/minimal';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from '$app/server';
+import { db } from '$lib/server/db';
+import { redis } from 'bun';
+
+export const auth = betterAuth({
+	appName: 'MicroManus',
+	baseURL: env.ORIGIN,
+	secret: env.BETTER_AUTH_SECRET,
+	database: drizzleAdapter(db, { provider: 'pg' }),
+	secondaryStorage: {
+		get: async (key) => {
+			const value = await redis.get(key);
+			if (value === 'owner') {
+				console.error(`[DEBUG] Redis key "${key}" has value "owner"!`);
+			}
+			return value;
+		},
+		set: async (key, value, ttl) => {
+			if (ttl) {
+				await redis.set(key, value, 'EX', ttl);
+			} else {
+				await redis.set(key, value);
+			}
+		},
+		delete: async (key) => {
+			await redis.del(key);
+		}
+	},
+
+	emailAndPassword: { enabled: false },
+	socialProviders: {
+		github: {
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET
+		}
+	},
+	plugins: [
+		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
+	],
+	advanced: {
+		cookiePrefix: 'micromanus',
+		ipAddress: {
+			ipAddressHeaders: ['cf-connecting-ip', 'x-forwarded-for']
+		}
+	}
+});
